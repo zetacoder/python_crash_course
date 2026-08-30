c6db083fff7d40f74a7b8d62ba4df35a73aaa4d1
zetacoder <amitsingh1.6180@gmail.com>
2026-08-29 20:50:31 -0500

    placeholder for 4_1 and first half of the chapter 6 exercises

---
+++ 1_Basics/6_Dictionaries/6_2_favorite_numbers.py
@@ -0,0 +1,9 @@
+favorite_numbers = {
+                        "person_1" : 3,
+                        "person_2" : 2,
+                        "person_3" : 6,
+                        "person_4" : 7
+                   }
+
+for person, favorite_number in favorite_numbers.items():
+    print(f"{person}'s favorite number is {favorite_number}")
---
+++ 1_Basics/6_Dictionaries/6_3_glossary.py
@@ -0,0 +1,11 @@
+glossary = {
+             "for loop" : "For loop helps in creating iteration of repetitive tasks",
+             "items()" : "items() returns a sequence of key value pair stored within the python dictionary",
+             "keys()" : "keys() returns the sequence of all the keys stored within a python dictionary",
+             "value()" : "value() returns the sequence of all the values stored within a python dictionary",
+             "set()" : "set is hybrid of list and dictionary and it only returns unique items stored",
+           }
+
+for word, meaning in glossary.items():
+    print(word, ":", meaning)
+
